222c47eae899adb7c793a4dab5cd04a412dd21a9
Dunya Kirkali <dunyakirkali@gmail.com>
2017-02-10 23:38:08 +0100

    Create an Single View Swift Project

---
+++ brws-mv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  brws-mv
+//
+//  Created by Dunya Kirkali on 2/10/17.
+//  Copyright © 2017 Dunya Kirkali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
