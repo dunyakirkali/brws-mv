eed110a0e90d2b1c38e3be3d1375b466830c99f3
Dunya Kirkali <dunyakirkali@gmail.com>
2017-02-11 22:19:09 +0100

    Add empty Movie model

---
+++ brws-mv/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  brws-mv
+//
+//  Created by Dunya Kirkali on 2/11/17.
+//  Copyright © 2017 Dunya Kirkali. All rights reserved.
+//
+
+import UIKit
+
+class Movie: NSObject {
+
+}
